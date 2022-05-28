54ea0dee5b0700400c03b2b625b441220915121d
magnanix-dev <amindtheatre@gmail.com>
2022-05-29 01:54:00 +1000

    Add freelook camera for testing. Clean generation and add mesh generation. Preparing for wall/ceiling generation.

--- assets/scripts/world/Generator.gd
+++ assets/scripts/world/Generator.gd
@@ -1,43 +1,60 @@
-extends Node
+extends Spatial
 class_name Generator
 
 export var size = Vector2(30, 30)
 export var cell_size = 32
 
 export var ground_maximum = 110
+export var ground_minimum = 70
 
 export var walkers_maximum = 5
-export var walkers_create_chance = 0.1
-export var walkers_delete_chance = 0.1
+export var walkers_create_chance = 0.02
+export var walkers_delete_chance = 0.01
+export var walkers_distance = 5
 export var walkers_2x_chance = 0.5
 export var walkers_3x_chance = 0.0
 
+export var ground_material : SpatialMaterial
 
-enum tile {ground, wall, none}
+enum tile {ground, ceiling, none}
 var grid
+
+onready var meshes = $Mesh
 
 var generate = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
+	#VisualServer.set_debug_generate_wireframes(true)
+	#get_viewport().debug_draw = Viewport.DEBUG_DRAW_WIREFRAME
 	yield(generate_map(), "completed")
 	print("Completed!")
+	var build_meshes = build_map()
+	if build_meshes.size():
+		for build in build_meshes:
+			generate_mesh(build)
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		get_tree().reload_current_scene()
 
 func generate_map():
+	generate = true
 	grid = array2d(size.x, size.y, tile.none)
 	var midpoint = Vector2(ceil(size.x/2), ceil(size.y/2))
 	var walkers = []
 	var removal = []
 	var iterations = 0
 	# Initial Walker:
-	walkers.append(Walker.new(midpoint, Vector2(0, size.x), Vector2(0, size.y)))
+	walkers.append(Walker.new(midpoint, Vector2(0, size.x-1), Vector2(0, size.y-1), walkers_distance))
 	while generate:
 		iterations += 1
 		for w in walkers:
 			var p = w.step()
-			grid[p.x][p.y] = tile.ground
+			if p: grid[p.x][p.y] = tile.ground
 			if randf() <= walkers_create_chance and walkers.size() < walkers_maximum:
-				walkers.append(Walker.new(p, Vector2(0, size.x), Vector2(0, size.y)))
+				walkers.append(Walker.new(p, Vector2(0, size.x), Vector2(0, size.y), walkers_distance))
 			if randf() <= walkers_delete_chance:
 				removal.append(w)
 		if removal.size():
@@ -45,14 +62,85 @@ func generate_map():
 				walkers.erase(w)
 				w.queue_free()
 			removal.clear()
-		if count_ground() >= ground_maximum:
+		if count_ground() >= ground_maximum or walkers.size() <= 0:
 			generate = false
 	if not generate and walkers.size():
 		for w in walkers:
 			w.queue_free()
 		walkers.clear()
-	print(iterations)
+	print("Iterations: ", iterations)
+	print("Ground Count: ", count_ground())
+	if count_ground() <= ground_minimum:
+		print("Failed! - Restarting!")
+		yield(generate_map(), "completed")
+#	for x in range(1, size.x-1):
+#		for y in range(1, size.y-1):
+#			if grid[x][y] == tile.ground:
+#				# check surrounds
 	yield(get_tree(), "idle_frame")
+
+func build_map():
+	var ground_verts = PoolVector3Array()
+	var wall_verts = PoolVector3Array()
+	var ceiling_verts = PoolVector3Array()
+	for x in range(size.x):
+		for y in range(size.y):
+			match grid[x][y]:
+				tile.ground:
+					var z = 0
+					ground_verts.append(Vector3(x, z, y))
+					ground_verts.append(Vector3((x + 1), z, y))
+					ground_verts.append(Vector3((x + 1), z, (y + 1)))
+					ground_verts.append(Vector3(x, z, (y + 1)))
+				tile.ceiling:
+					var z = 1
+					ceiling_verts.append(Vector3(x, z, y))
+					ceiling_verts.append(Vector3((x + 1), z, y))
+					ceiling_verts.append(Vector3((x + 1), z, (y + 1)))
+					ceiling_verts.append(Vector3(x, z, (y + 1)))
+	
+	return [ground_verts, wall_verts, ceiling_verts]
+
+func generate_mesh(verts):
+	var mesh_instance = MeshInstance.new()
+	meshes.add_child(mesh_instance)
+	var mesh = ArrayMesh.new()
+
+	var arr = []
+	arr.resize(Mesh.ARRAY_MAX)
+	
+	var uvs = PoolVector2Array()
+	var normals = PoolVector3Array()
+	var tangents = PoolRealArray()
+	var indices = PoolIntArray()
+	for n in range(0, verts.size(), 4):
+		indices.append(n)
+		indices.append(n+2)
+		indices.append(n+3)
+		indices.append(n)
+		indices.append(n+1)
+		indices.append(n+2)
+		uvs.append(Vector2(0, 0))
+		uvs.append(Vector2(1, 0))
+		uvs.append(Vector2(1, 1))
+		uvs.append(Vector2(0, 1))
+	for n in range(0, verts.size()):
+		normals.append(Vector3.UP)
+		tangents.append_array([-1,0,0,1])
+
+	# Assign arrays to mesh array.
+	arr[Mesh.ARRAY_VERTEX] = verts
+	arr[Mesh.ARRAY_TEX_UV] = uvs
+	arr[Mesh.ARRAY_NORMAL] = normals
+	arr[Mesh.ARRAY_TANGENT] = tangents
+	arr[Mesh.ARRAY_INDEX] = indices
+
+	# Create mesh surface from mesh array.
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arr)
+	mesh_instance.mesh = mesh
+	#mesh_instance.material_override = ground_material
+	#mesh_instance.create_trimesh_collision()
+	#mesh_instance.visible = true
 
 func count_ground():
 	var ground_count = 0
--- assets/scripts/world/Walker.gd
+++ assets/scripts/world/Walker.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name Walker
 
-export var viable_directions = [Vector2.UP, Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT]
+export var can_reverse = true
 export var turn_chance = 0.25
 export var max_dist = 10
 
@@ -11,19 +11,27 @@ var pos = Vector2.ZERO
 var dir = Vector2.RIGHT
 var steps = []
 var dist = 0
+var direction = {
+	Vector2.RIGHT: [Vector2.DOWN, Vector2.UP],
+	Vector2.LEFT: [Vector2.UP, Vector2.DOWN],
+	Vector2.UP: [Vector2.RIGHT, Vector2.LEFT],
+	Vector2.DOWN: [Vector2.LEFT, Vector2.RIGHT],
+}
 
-func _init(start, size_x, size_y):
+func _init(start, size_x, size_y, distance):
 	x.min = size_x.x
 	x.max = size_x.y
 	y.min = size_y.x
 	y.max = size_y.y
 	assert(not out_of_bounds(start))
 	pos = start
+	max_dist = distance
 
 func step():
 	if randf() <= turn_chance or dist >= max_dist:
 		change_direction()
 	var target = pos + dir
+	#print(target.x, ", ", target.y)
 	if not out_of_bounds(target):
 		dist += 1
 		pos = target
@@ -33,18 +41,17 @@ func step():
 		step()
 
 func change_direction():
-	dist = 0
-	var directions = viable_directions.duplicate()
-	directions.erase(dir)
-	directions.shuffle()
-	dir = directions.pop_front()
+	var turn = direction[dir].duplicate()
+	if can_reverse: turn.append(dir * -1)
+	turn.shuffle()
+	dir = turn.pop_front()
 	while out_of_bounds(pos + dir):
-		dir = directions.pop_front()
+		dir = turn.pop_front()
 
 func out_of_bounds(target):
 	var _x = target.x
 	var _y = target.y
-	if _x < x.min || _x > x.max || _y < y.min || _y > y.max:
+	if _x <= x.min || _x >= x.max || _y <= y.min || _y >= y.max:
 		return true
 	else:
 		return false
